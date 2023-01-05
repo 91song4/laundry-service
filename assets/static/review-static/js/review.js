0ae8f488ab3b02f3c1b269358405c0b007061df4
Kim taeyeon <tea.utt0903@gmail.com>
2023-01-06 04:47:21 +0900

    taeyeon-tenth

--- assets/static/review-static/js/review.js
+++ assets/static/review-static/js/review.js
@@ -1,10 +1,13 @@
+// const { template } = require("@babel/core");
+// const { listen } = require("engine.io");
+
 const drawStar = (target) => {
 document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
 
 }
  
 
-function create_review(review_id) {
+function create_review() {
   const star = $('.star_rate').val();
   // file,
   const content =  $('#review_content').val();
@@ -12,12 +15,15 @@ function create_review(review_id) {
 
   $.ajax({
     type: 'POST',
-    url:`/api/reviews/${review_id}`,
+    url:`/api/reviews/6`,
     data: {
       star: star,
       review2: content
     },
     dataType: 'json',
-    // success:
+     // 완료 시 페이지 이동
+    success: function (response) {
+        window.location.replace('../../../templates/review-list.html');
+     }
   });
 }
